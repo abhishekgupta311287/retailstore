15e18a040cc10104bec1977290c6d8f2c3f59c4b
abhishekviu <abhishek.gupta@vuclip.com>
2021-01-30 16:40:25 +0530

    Added RetailStoreViewModel implementation

--- app/src/main/java/com/abhishek/retailstore/ui/main/RetailStoreViewModel.kt
+++ app/src/main/java/com/abhishek/retailstore/ui/main/RetailStoreViewModel.kt
@@ -2,8 +2,38 @@ package com.abhishek.retailstore.ui.main
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.abhishek.retailstore.model.Cart
 import com.abhishek.retailstore.model.Product
+import com.abhishek.retailstore.repo.db.IRetailStoreRepo
+import kotlinx.coroutines.launch
 
-class RetailStoreViewModel : ViewModel() {
-    val selectedProduct:MutableLiveData<Product> = MutableLiveData()
+class RetailStoreViewModel(private val repo: IRetailStoreRepo) : ViewModel() {
+    val selectedProduct: MutableLiveData<Product> = MutableLiveData()
+    val cartListLiveData: MutableLiveData<List<Cart>> = MutableLiveData()
+
+    fun addToCart(product: Product) {
+        viewModelScope.launch {
+
+            repo.insertOrUpdate(
+                Cart(
+                    product
+                )
+            )
+        }
+
+    }
+
+    fun fetchCart() {
+        viewModelScope.launch {
+            cartListLiveData.value = repo.getProductsFromCart()
+        }
+    }
+
+    fun deleteCartProduct(cart: Cart) {
+        viewModelScope.launch {
+            repo.deleteFromCart(cart)
+            cartListLiveData.value = repo.getProductsFromCart()
+        }
+    }
 }
